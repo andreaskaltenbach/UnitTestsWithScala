c7d9ff36d7daa132e2e0e85afdb49ba3bc5679e8
Andreas Kaltenbach <andreaskaltenbach@Andreas-Kaltenbachs-MacBook-Pro.local>
2011-01-23 18:15:15 +0100

    collection tests

--- src/test/scala/com/stendahls/ScalaCelebrityTest.scala
+++ src/test/scala/com/stendahls/ScalaCelebrityTest.scala
@@ -27,19 +27,4 @@ class ScalaCelebrityTest {
     createCelebrityList foreach(println(_))
     createNo1UkHitsMap foreach(println(_))
   }
-
-
-   /*
-  @Test def testCelebrities() = {
-  val celebrities = List(
-Person("Michael Jackson", 1958, 2009),
-Person("Madonna", 1958),
-Person("Jesus", deathYear=31))
-
-  val no1HitsUk = Map(
-7 -> Person("Michael Jackson", 1958, 2009),
-13 -> Person("Madonna", 1958),
-0 -> Person("Jesus", deathYear=31))
-  …
-}*/
 }
