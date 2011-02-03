7ac937df4f63fccda79a80066fe6731cde341037
Andreas Kaltenbach <andreaskaltenbach@Andreas-Kaltenbachs-MacBook-Pro.local>
2011-02-03 14:58:21 +0100

    resurrection test in scala

---
+++ src/test/scala/com/stendahls/ResurrectionTest.scala
@@ -0,0 +1,59 @@
+package com.stendahls
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Test
+import service.{CelebrityAlreadyAliveException, CelebrityServiceImpl}
+import support.Celebrity
+import support.Celebrity._
+import scala.collection.JavaConversions._
+
+/**
+ * 
+ * @author andreaskaltenbach
+ */
+
+class ResurrectionTest extends AssertionsForJUnit {
+
+  val celebrityService = new CelebrityServiceImpl
+
+  /**
+   * 1. Resurrect Michael Jackson
+   */
+  @Test def testResurrectMichaelJackson() {
+    val michael = Celebrity("Michael Jackson", 1958, 2009)
+
+    assert(!michael.isAlive)
+
+    celebrityService.resurrect(Set(michael))
+
+    assert(michael.isAlive)
+  }
+
+  /**
+   * 2. Try to resurrect Madonna, but she is already alive
+   */
+  @Test def testResurrectMadonna() {
+    val madonna = Celebrity("Madonna", 1958)
+
+    intercept[CelebrityAlreadyAliveException] {
+		  celebrityService.resurrect(Set(madonna))
+		}
+  }
+
+  /**
+   * 3. Resurrect Michael Jackson and Jesus and verify
+   *    explicitly that those two are really alive again
+   */
+  @Test def testResurrectMichaelAndJesus() {
+    val celebrities = Set(
+      Celebrity("Michael Jackson", 1958, 2009),
+      Celebrity("Jesus", deathYear=31))
+
+    assert(celebrities.forall(c => !c.isAlive))
+
+    celebrityService.resurrect(celebrities)
+
+    assert(celebrities.exists(c => c.getName == "Michael Jackson" && c.isAlive))
+    assert(celebrities.exists(c => c.getName == "Jesus" && c.isAlive))
+  }
+}
