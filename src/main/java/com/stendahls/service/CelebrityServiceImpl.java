ac63e85c72816808a3b933f204874b2fa8aa5c02
Andreas Kaltenbach <andreaskaltenbach@Andreas-Kaltenbachs-MacBook-Pro.local>
2011-02-03 11:44:56 +0100

    make validity checks on celebrities

--- src/main/java/com/stendahls/service/CelebrityServiceImpl.java
+++ src/main/java/com/stendahls/service/CelebrityServiceImpl.java
@@ -9,6 +9,12 @@ public class CelebrityServiceImpl implements CelebrityService {
 
     @Override
     public Celebrity createCelebrity(Celebrity celebrity) {
+        if (celebrity.getName() == null || celebrity.getName().isEmpty()) {
+            throw new IllegalArgumentException("Name not set");
+        }
+        if (celebrity.getBirthYear() > celebrity.getDeathYear()) {
+            throw new IllegalArgumentException("Birth year after death year");
+        }
         return celebrity;
     }
 }
