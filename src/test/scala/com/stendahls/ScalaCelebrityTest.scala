d8b04bd1d2a805ac340165558cf95fd74e9760f9
Andreas Kaltenbach <andreaskaltenbach@Andreas-Kaltenbachs-MacBook-Pro.local>
2011-02-12 14:18:15 +0100

    ^

--- src/test/scala/com/stendahls/ScalaCelebrityTest.scala
+++ src/test/scala/com/stendahls/ScalaCelebrityTest.scala
@@ -17,6 +17,7 @@ class ScalaCelebrityTest {
       new Celebrity("Jesus", 0, 31))
 
   def createNo1UkHitsMap =
+    //scala.collection.immutable.Map.apply(
     Map(
        7 -> new Celebrity("Michael Jackson", 1958, 2009),
       13 -> new Celebrity("Madonna", 1958, null),
